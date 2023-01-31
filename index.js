7738a1a18cf802f197d74e9a06df4e54f4374384
aoperat <54986046+aoperat@users.noreply.github.com>
2023-01-31 16:43:14 +0900

    처음 저장소에 올림"

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.set('strictQuery',true)
+
+mongoose.connect('mongodb+srv://user:1234@clusternodetodo.hbqeu.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
