2d278bbfcd76eef0fba714de44cd87471fc8afc6
aoperat <54986046+aoperat@users.noreply.github.com>
2023-02-01 14:59:05 +0900

    유저정보 db저장

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const port = 3000
 
 //const mongoose = require('mongoose')
 import mongoose from 'mongoose';
+
+import config from './config/dev.js'
+
+
 mongoose.set('strictQuery',true)
 
-mongoose.connect('mongodb+srv://user:1234@clusternodetodo.hbqeu.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>console.log('MongoDB Connected...'))
@@ -28,9 +32,11 @@ app.post('/register',(req,res)=>{
     
     //회원가입할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
-
-    const user = new User(req);
+    console.log(req.body)
+    const user = new User(req.body);
+    
     user.save((err,userInfo) =>{
+        console.log(userInfo)
         if(err) return res.json({success:false, err})
         return res.status(200).json({
             success: true
