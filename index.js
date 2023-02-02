f288ab5ccc83b3370798dd30c7668b64187548d1
aoperat <54986046+aoperat@users.noreply.github.com>
2023-02-02 13:23:25 +0900

    로그인 기능 생성(아이디, 비밀번호확인, 토큰생성)

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 import express from 'express'
 const app = express()
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+import cookieParser from 'cookie-parser';
+app.use(cookieParser());
 
 import User from "./model/User.js";
 //const {User} = require("./models/User")
@@ -11,33 +14,31 @@ const port = 3000
 
 //const mongoose = require('mongoose')
 import mongoose from 'mongoose';
-
 import config from './config/dev.js'
 
 
-mongoose.set('strictQuery',true)
+mongoose.set('strictQuery', true)
 
-mongoose.connect(config.mongoURI,{
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!')
+    res.send('Hello World!!')
 })
 
-app.post('/register',(req,res)=>{
-    
+app.post('/register', (req, res) => {
+
     //회원가입할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
     console.log(req.body)
     const user = new User(req.body);
-    
-    user.save((err,userInfo) =>{
-        console.log(userInfo)
-        if(err) return res.json({success:false, err})
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
         })
@@ -45,7 +46,37 @@ app.post('/register',(req,res)=>{
 
 })
 
+app.post('/login', (req, res) => {
+    //로그인 
+    //1. 요청된 이메일을 데이터베이스에서 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+        //2. 요청된 이메일이 있는 경우, 비밀번호가 맞는지 체크.
+        user.comparePassword(req.body.password, (err, isMatched) => {
+            if (!isMatched) {
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." });
+            }
+
+            //3. 비밀번호 일치시 토큰 생성.
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // 토큰 저장. 어디에? 쿠키, 로컬스토리지 중 쿠키
+
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+            })
+        })
+    })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
--- model/User.js
+++ model/User.js
@@ -1,5 +1,12 @@
 //const mongoose = require('mongoose');
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+//var jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken'
+const saltRounds = 10;
+
+
+
 
 const userSchema = mongoose.Schema({
     name:{
@@ -31,6 +38,60 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save',function(next){
+
+    
+    var user = this;
+    //비밀번호 암호화
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err) return next(err);
+                user.password = hash;
+                console.log(user.password)
+                next();
+            });
+        });
+    }else{
+        next();
+    }
+})
+
+
+
+
+userSchema.methods.generateToken = function(callback){
+
+    var user = this;
+    //jsonwebtoken을 이용해서 token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token;
+    user.save(function(err, user){
+        if(err) return callback(err);
+        callback(null, user);
+         
+    })
+    //user._id + 'secretToken' = token
+
+}
+
+userSchema.methods.comparePassword = function(plainPassword, callback){
+    
+    //plainPassword 
+    bcrypt.compare(plainPassword, this.password, function(err, isMatched){
+        if(err) return callback(err);
+
+        callback(null, isMatched);
+
+    })
+
+}
 
 const User = mongoose.model('User',userSchema)
 //module.exports = {User}
